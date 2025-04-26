7e7d83e515e228f9a4779d5587bab0f0ee7e2231
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-04-26 14:23:52 -0300

    se logro mostrar en la tabla catidad de veces que sale una suma al arrajar los dados

--- js/03-dados.js
+++ js/03-dados.js
@@ -4,21 +4,25 @@ uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados
 y anotar en un array el número de apariciones de dicha suma, repitiendo 50 
 veces esta operación.*/
 
+const contadora = new Array(13).fill(0);
+
+for (let i = 0; i <= 50; i++) {
+  let dado1 = Math.floor(Math.random() * 6 + 1);
+  let dado2 = Math.floor(Math.random() * 6 + 1);
+  let suma = dado1 + dado2;
+  contadora[suma]++;
+}
+
 document.writeln(`<table>`);
-    document.writeln(`<thead>`);
-        document.writeln(`<tr>`);
-            document.writeln(`<th>Suma 🎲🎲</th>`);
-            document.writeln(`<th>Apariciones</th>`);
-        document.writeln(`</tr>`);
-    document.writeln(`</thead>`);
-        document.writeln(`<tbody>`);
-        document.writeln(`<tr>`);
-            document.writeln(`<td>Dato 1</td>`);
-            document.writeln(`<td>Dato 2</td>`);
-        document.writeln(`</tr>`);
-        document.writeln(`<tr>`);
-            document.writeln(`<td>Dato 1</td>`);
-            document.writeln(`<td>Dato 2</td>`);
-        document.writeln(`</tr>`);
-    document.writeln(`</tbody>`);
+document.writeln(
+  "<thead><tr><th>Suma 🎲🎲</th><th>Apariciones</th></tr></thead>"
+);
+document.writeln(`<tbody>`);
+for (let suma = 2; suma <= 12; suma++) {
+  document.writeln(`<tr>`);
+  document.writeln(`<td>${suma}</td>`);
+  document.writeln(`<td>${contadora[suma]}</td>`);
+  document.writeln(`</tr>`);
+}
+document.writeln(`</tbody>`);
 document.writeln(`</table>`);
