b92178f89f0caa9cdc88e23e0b325d8677c9dde3
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-04-26 13:28:08 -0300

    se agrego la estructura base de la tabla

---
+++ js/03-dados.js
@@ -0,0 +1,24 @@
+/* 3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de 
+la función Math.random para obtener números aleatorios entre 1 y 6 para cada 
+uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados 
+y anotar en un array el número de apariciones de dicha suma, repitiendo 50 
+veces esta operación.*/
+
+document.writeln(`<table>`);
+    document.writeln(`<thead>`);
+        document.writeln(`<tr>`);
+            document.writeln(`<th>Suma 🎲🎲</th>`);
+            document.writeln(`<th>Apariciones</th>`);
+        document.writeln(`</tr>`);
+    document.writeln(`</thead>`);
+        document.writeln(`<tbody>`);
+        document.writeln(`<tr>`);
+            document.writeln(`<td>Dato 1</td>`);
+            document.writeln(`<td>Dato 2</td>`);
+        document.writeln(`</tr>`);
+        document.writeln(`<tr>`);
+            document.writeln(`<td>Dato 1</td>`);
+            document.writeln(`<td>Dato 2</td>`);
+        document.writeln(`</tr>`);
+    document.writeln(`</tbody>`);
+document.writeln(`</table>`);
